36e93db9541df20b3c57a09a03234ec304a450cc
Ha Young Jin <44938459+HYJ29@users.noreply.github.com>
2020-07-10 11:59:35 +0900

    Staged (#20)
    * link to stage api server
    
    * edit staged url
    
    * use REACT_APP_NODE_ENV to use after build

--- src/api/client.js
+++ src/api/client.js
@@ -3,13 +3,15 @@ import axios from "axios";
 import { apiRequestInterceptor, apiResponseInterceptor } from "./helpler";
 import { apiServer } from "config";
 
-const env = process.env.NODE_ENV;
+const env = process.env.REACT_APP_NODE_ENV;
 console.log("env", env);
 const creatorLinkCname = apiServer.production;
 const local = apiServer.development;
 let url = local;
 if (env === "production") {
   url = creatorLinkCname;
+} else if (env === "staged") {
+  url = apiServer.staged;
 }
 
 const apiClient = axios.create({

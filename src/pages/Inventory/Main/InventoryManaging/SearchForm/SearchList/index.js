c78b88b0c007344ec284090750af865f78547f33
Ha Young Jin <44938459+HYJ29@users.noreply.github.com>
2020-07-10 12:57:44 +0900

    add scroll feature on ingredients (#22)

--- src/pages/Inventory/Main/InventoryManaging/SearchForm/SearchList/index.js
+++ src/pages/Inventory/Main/InventoryManaging/SearchForm/SearchList/index.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import styled from "styled-components";
+
+import { selectors } from "data";
 
 import Ingredient from "./Ingredient";
 
 export default function SearchListComp({ searchList }) {
+  const isCurrentIngredientExist =
+    useSelector(selectors.inventory.getCurrentIngredient) !== null;
   return (
-    <Container>
+    <Container isCurrentIngredientExist={isCurrentIngredientExist}>
       {searchList.map(ingredient => (
         <Ingredient ingredient={ingredient} key={ingredient.id} />
       ))}
@@ -18,5 +23,12 @@ const Container = styled.div`
   flex-wrap: wrap;
   align-items: flex-start;
   align-content: flex-start;
-  min-height: 150px;
+  height: ${({ isCurrentIngredientExist }) =>
+    isCurrentIngredientExist ? "145px" : "300px"};
+
+  overflow: scroll;
+  padding: 3px;
+  border-radius: 10px;
+  border: 1px solid gray;
+  margin-bottom: 5px;
 `;
